8513a7a85829079b8dfd1820bfb964ebe89dbf2a
Nicholas Santiago <nicholas@santiago.wtf>
2022-01-02 14:52:48 -0500

    aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -2,5 +2,10 @@
 
 alias reload='exec ${SHELL}'
 alias tf="terraform"
+
 alias sshnick="ssh -i ${HOME}/.ssh/nick"
 alias sshltel="ssh -i ${HOME}/.ssh/ltel"
+
+alias sshdev="ssh -i ${HOME}/.ssh/cds-dev"
+alias sshtest="ssh -i ${HOME}/.ssh/cds-test"
+alias sshprod="ssh -i ${HOME}/.ssh/cds-prod"
